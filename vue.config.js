ed0717b44386b03850130a04644bb56070e848a2
tunjixy <kendricktunji@gmail.com>
2020-08-08 13:12:19 +0100

    Modify vue.config file

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,11 @@
-const webpack = require('webpack');
+const webpack = require('webpack')
 
 module.exports = {
-  configureWebpack: {
-    // Set up all the aliases we use in our app.
-    plugins: [
-      new webpack.optimize.LimitChunkCountPlugin({
-        maxChunks: 6
-      })
-    ]
-  },
   pwa: {
-    name: 'Design Snitch',
-    themeColor: '#F76E4E',
-    msTileColor: '#F76E4E',
+    name: 'Crud Crud',
+    themeColor: '#69da55',
+    msTileColor: '#69da55',
     appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: '#F76E4E'
-  },
-  css: {
-    // Enable CSS source maps.
-    sourceMap: process.env.NODE_ENV !== 'production'
+    appleMobileWebAppStatusBarStyle: '#69da55'
   }
-};
+}
